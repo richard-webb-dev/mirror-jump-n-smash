da3ff352c2e75441d94ae5d1563df7c48baa3ceb
Sandroleon313 <sandroflei313@gmail.com>
2023-11-25 17:53:21 +0100

    version 0.1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-using UnityEngine;
+
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject gameOverPanel;
+    public TMP_Text gameOverText;
+
+    public int score = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -18,16 +22,39 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if(score >= 7)
+        {
+            showGameOverScreen(true);
+        }
+        if(score <= -7)
+        {
+            showGameOverScreen(false);
+        }
     }
 
-    public void showGameOverScreen()
+    public void showGameOverScreen(bool playerOneWin)
     {
+        if (playerOneWin)
+        {
+            gameOverText.text = "Playey One WINS!";
+
+        }
+        if (!playerOneWin)
+        {
+            gameOverText.text = "Player Two WINS!";
+        }
+        
         gameOverPanel.SetActive(true);
         Debug.Log("Game Over");
+        Time.timeScale = 0f;
     }
     public void RestartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void changeScore(int change)
+    {
+        score += change;
     }
 }
