e6b4a1d74d4a0d18815a63a95abf5bc50c8cbff7
Sandroleon313 <sandroflei313@gmail.com>
2023-11-25 15:23:40 +0100

    Fixed Unified Controller

--- Assets/Scripts/PlayerOne.cs
+++ Assets/Scripts/PlayerOne.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerOne : MonoBehaviour
 {
@@ -12,29 +13,54 @@ public class PlayerOne : MonoBehaviour
 
     public bool grounded = false;
 
+    public UnityEvent bulletHit;
+
+    public enum player
+    {
+        one, two
+    }
+
+    public player p;
+
+    private KeyCode left;
+    private KeyCode right;
+    private KeyCode up;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        if(p == player.one)
+        {
+            left = KeyCode.A;
+            right = KeyCode.D;
+            up = KeyCode.W;
+        }
+        if (p == player.two)
+        {
+            left = KeyCode.LeftArrow;
+            right = KeyCode.RightArrow;
+            up = KeyCode.UpArrow;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        if (Input.GetKey("d"))
+        if (Input.GetKey(right))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
 
         }
-        if (Input.GetKey("a"))
+        if (Input.GetKey(left))
         {
             transform.Translate(-speed * Time.deltaTime, 0, 0);
 
         }
 
-        if (Input.GetKeyDown("w") && grounded)
+        if (Input.GetKeyDown(up) && grounded)
         {
             rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
         }
@@ -56,4 +82,12 @@ public class PlayerOne : MonoBehaviour
             grounded = false;
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "´Bullet")
+        bulletHit.Invoke();
+    }
+
+
 }
